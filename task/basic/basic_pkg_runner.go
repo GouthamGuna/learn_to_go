0fe01df8cadb73a6415557899eac2bc24488a8cf
Goutham <ggowthamsankar@gmail.com>
2024-05-21 15:46:00 +0530

    initial commit

---
+++ task/basic/basic_pkg_runner.go
@@ -0,0 +1,23 @@
+package basic
+
+func Basic_PKG_Runner() {
+
+	// external pkg
+	QuotePrint()
+
+	// Arrays
+	ExerciseOne()
+	ExerciseTwo()
+	ExerciseThree()
+
+	// Operators
+	ExerciseFour()
+	ExerciseFive()
+	ExerciseSix()
+
+	// Conditions
+	ExerciseSeven()
+	ExerciseEight()
+	ExerciseNine()
+	ExerciseTen()
+}

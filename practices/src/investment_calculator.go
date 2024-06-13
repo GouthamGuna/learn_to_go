f822a5dd3c47cf30730ae58986989b69aba3f424
GouthamGuna <ggowthamsankar@gmail.com>
2024-06-13 21:38:14 +0530

    Update investment_calculator.go

--- practices/src/investment_calculator.go
+++ practices/src/investment_calculator.go
@@ -6,30 +6,31 @@ import (
 )
 
 const RatioBy float64 = 100.0
+const inflationRate = 6.5
 
 func InvestmentCalculator() {
-
-	const inflationRate = 6.5
-
-	var investmentAmount float64
-	var years float64
-
 	expectedReturnRate := 5.5
 
-	fmt.Print("Enter Investing Amount : ")
-	fmt.Scan(&investmentAmount)
+	investmentAmount := fetchResourceFromUser("Enter Investing Amount : ")
+	years := fetchResourceFromUser("Enter Year : ")
+	expectedReturnRate = fetchResourceFromUser("Enter Expected Return Rate : ")
 
-	fmt.Print("Enter Year : ")
-	fmt.Scan(&years)
-
-	fmt.Print("Enter Expected Return Rate : ")
-	fmt.Scan(&expectedReturnRate)
-
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/RatioBy, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := doCalculate(investmentAmount, expectedReturnRate, years)
 
 	formattedFV := fmt.Sprintf("Future Value: %.2f", futureValue)
 	formattedRFV := fmt.Sprintf("\nFuture Value (adjusted for Inflation): %.2f\n", futureRealValue)
 
 	fmt.Println(formattedFV, formattedRFV)
+}
+
+func doCalculate(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/RatioBy, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	return futureValue, futureRealValue
+}
+
+func fetchResourceFromUser(infoText string) (responseValue float64) {
+	fmt.Print(infoText)
+	fmt.Scan(&responseValue)
+	return responseValue
 }

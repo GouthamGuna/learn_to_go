4dc3b233d073fad133e2f36c4eeedc6a4b18c865
GouthamGuna <ggowthamsankar@gmail.com>
2024-06-14 19:49:24 +0530

    Update runner.go

--- practices/src/runner.go
+++ practices/src/runner.go
@@ -7,27 +7,35 @@ import (
 
 func ApplicationRunner() {
 	limits := 99
+	fmt.Println("\nWelcome to the go programming language practice session")
 
 	for limits < 100 {
 
 		var options int
 
-		fmt.Println("Welcome to the go programming language practice session")
-		fmt.Print("\nPress 1 : Investment Calculator \nPress 2 : Profit Calculator \nPress 3 : Quit \nEnter the values : ")
+		fmt.Print("\nPress 1 : Investment Calculator \nPress 2 : Profit Calculator \nPress 3 : Quit \n\nEnter the values : ")
 
 		fmt.Scan(&options)
 
 		switch options {
 		case (1):
 			fmt.Println(" ")
+			LogWritter("Invoking the InvestmentCalculator() fun...")
 			InvestmentCalculator()
 		case (2):
 			fmt.Println(" ")
+			LogWritter("Invoking the ProfitCalculator() fun...")
 			ProfitCalculator()
 		case (3):
-			os.Exit(0)
+			return
 		default:
+			LogWritter("Invoking the default block...")
 			fmt.Println("Invalid Option")
+			continue
 		}
 	}
+}
+
+func LogWritter(userLog string) {
+	os.WriteFile("go_app.log", []byte(userLog), 0644)
 }

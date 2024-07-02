b1b0d1472551225ccdc469108b78608d58d1ee21
Goutham <ggowthamsankar@gmail.com>
2024-07-02 12:50:48 +0530

    Update plus_minus.go

--- skill_test/src/problem_solving/plus_minus.go
+++ skill_test/src/problem_solving/plus_minus.go
@@ -2,16 +2,17 @@ package problemsolving
 
 import (
 	"errors"
+	"fmt"
 )
 
-func algorithmPlusMinus(arr []int64) ([3]float64, error) {
+func plusMinus(arr []int64) ([3]float64, error) {
 
 	if len(arr) <= 0 {
 		return [3]float64{}, errors.New("the input array must contain at least one element")
 	}
 
 	var positive, negative, zero int64
-	
+
 	for _, num := range arr {
 		if num > 0 {
 			positive++
@@ -28,4 +29,16 @@ func algorithmPlusMinus(arr []int64) ([3]float64, error) {
 		float64(negative) / totalElements,
 		float64(zero) / totalElements,
 	}, nil
+}
+
+func run_algorithmPlusMinus() {
+
+	result, err := plusMinus([]int64{1, 1, 0, -1, -1})
+
+	if err != nil {
+		fmt.Println("Error : ", err)
+		return
+	}
+
+	fmt.Printf(" Positive fraction: %.6f\n Negative fraction: %.6f\n Zero fraction: %.6f\n ", result[0], result[1], result[2])
 }

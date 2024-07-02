75de8286f4856fa7e3e5887dd0060ce33a5f31ac
Goutham <ggowthamsankar@gmail.com>
2024-07-02 12:50:29 +0530

    Update pkg_runner.go

--- skill_test/src/problem_solving/pkg_runner.go
+++ skill_test/src/problem_solving/pkg_runner.go
@@ -1,15 +1,8 @@
 package problemsolving
 
-import "fmt"
-
 func PS_PackageRunner() {
 
-	result, err := algorithmPlusMinus([]int64{1, 1, 0, -1, -1})
-
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	fmt.Printf(" Positive fraction: %.6f\n Negative fraction: %.6f\n Zero fraction: %.6f\n ", result[0], result[1], result[2])
+	run_algorithmPlusMinus()
+	run_findMinMaxSum()
+	run_isPalindrome()
 }
